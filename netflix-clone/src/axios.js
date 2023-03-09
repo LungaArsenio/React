d3c44181384d12f4747e939cd6bf998898b38490
lunga <lungaarsenio0@gmail.com>
2023-03-10 00:40:59 +0100

    Cleaning the files in the app and created the bones to start making requests

---
+++ netflix-clone/src/axios.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+/** base url to make requests to the movie database */
+
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+});
+
+export default instance;
+
+/**
+ * A method is a block of code that only runs when it is called,
+ * you can pass data into. (parameters).
+ *
+ *                      @Axios
+ * Axios let us create custom instances for API request.
+ * Each instance is a separate client that an carry its own configuration and options
+ * like baseURLs, timeouts, and headers.
+ * Then we can reuse the configuration for API calls usng the same instance.
+ *
+ * The axios.create() function creates a new Axios instance.
+ * The reason why are created instances is to set custom defaults
+ * for your application.
+ *
+ */
