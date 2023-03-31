7a9b79db0324e85b0430d18517f8a8a3c60f5ccf
lunga <lungaarsenio0@gmail.com>
2023-04-01 00:50:37 +0100

    adding the trailer

--- netflix-clone/src/Row.jsx
+++ netflix-clone/src/Row.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // A snippet of code which runs based on a specific condition/variable
   useEffect(() => {
@@ -17,6 +20,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]); //fetchUrl here function as a dependence because we're using a variable which is being passed from outside of the block.
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  // ATTENTION
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+  // Attention
   return (
     <div className="row">
       <h1>{title}</h1>
@@ -27,6 +53,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"} `}
             src={`${baseUrl}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -35,6 +62,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
@@ -49,7 +77,7 @@ export default Row;
  *
  * .get is basically apending this "fetchUrl" to the actual link
  *
- * @LINE31
+ * @LINE41
  * That line of code works because we created a prop that is set to true,
  * and we put it to add a class "row_posterLarge" if the return of the function
  * were true. Since we are using AND Operator (&&) it will return true because our prop "isLargeRow"
