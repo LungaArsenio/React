ee7702c3b0fc0fd702683b13100fe6b09bfd28f4
lunga <lungaarsenio0@gmail.com>
2023-03-31 19:41:28 +0100

    little changes

--- netflix-clone/src/Row.jsx
+++ netflix-clone/src/Row.jsx
@@ -1,43 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import axios from './axios';
-import "./Row.css"
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-const baseUrl = "https://image.tmdb.org/t/p/original/"
+const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row( { title, fetchUrl, isLargeRow } ) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   // A snippet of code which runs based on a specific condition/variable
   useEffect(() => {
     async function fetchData() {
-        const request = await axios.get(fetchUrl);
-        setMovies(request.data.results);
-        return request;
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
     }
     fetchData();
-  }, [fetchUrl]);//fetchUrl here function as a dependence because we're using a variable which is being passed from outside of the block.
-
+  }, [fetchUrl]); //fetchUrl here function as a dependence because we're using a variable which is being passed from outside of the block.
 
   return (
     <div className="row">
-        <h1>{title}</h1>
+      <h1>{title}</h1>
 
-        <div className='row__posters'>
-            {/* several row posters */}
+      <div className="row__posters">
+        {/* several row posters */}
 
-            {movies.map((movie) => (
-                <img
-                key={movie.id}
-                className={`row__poster ${isLargeRow && 'row__posterLarge'} `}
-                src={`${baseUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                alt={movie.name} />
-            ))}
-        </div>
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"} `}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
 
 /**
  * States is basically the way to write variables in react
