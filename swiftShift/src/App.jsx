035a63d2ad08ee72b858699589e53dc192688ca9
lunga <lungaarsenio0@gmail.com>
2023-04-18 20:14:38 +0100

    some changes

--- swiftShift/src/App.jsx
+++ swiftShift/src/App.jsx
@@ -1,4 +1,3 @@
-import Nav from "./components/Nav";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -10,19 +9,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/Features" element={<Features />}></Route>
-          <Route path="/About" element={<About />}></Route>
-          <Route path="/Blog" element={<Blog />}></Route>
-          <Route path="/Contact" element={<Contact />}></Route> */}
+          {/* <Route path="/Features" element={<Features />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/Contact" element={<Contact />} /> */}
         </Routes>
-        {/* Heading */}
-        {/* Huge Image */}
-        {/* Trusted by */}
-        {/* features */}
-        {/* pricing */}
-        {/* Card */}
-        {/* Whats new */}
-        {/* Footer */}
       </Router>
     </div>
   );
