948b9604bfeba6de302ebccbfb3b3f291215d495
lunga <lungaarsenio0@gmail.com>
2023-04-18 20:13:35 +0100

    header of the main page

---
+++ swiftShift/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css";
+
+function Header({ title, description }) {
+  return (
+    <div className="header">
+      <h1 className="header__title">{title}</h1>
+      <p className="header__description"> {description}</p>
+      <div className="header__btn">
+        <button className="header__btn--left">Join us now</button>
+        <button className="header__btn--right">Request demo</button>
+      </div>
+      <div className="header__imgContainer">
+        <img
+          className="header__img"
+          src="./src/assets/teampicture.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- swiftShift/src/pages/Home.jsx
+++ swiftShift/src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Nav from "../components/Nav";
+import "./Home.css";
+import Header from "../components/Header";
 
 function Home() {
   return (
     <>
       <Nav />
-      <h1>Home</h1>
+      <div className="row">
+        <div className="container">
+          <Header
+            title="SwiftShift"
+            description="Streamline Your Workflow with Our Secure, User-Friendly Product/Service - Seamlessly Integrated with Other Tools."
+          />
+        </div>
+      </div>
     </>
   );
 }
