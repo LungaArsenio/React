65ed369545d4d4f001c85a90b8f05d508cee3f3c
lunga <lungaarsenio0@gmail.com>
2023-04-23 00:50:58 +0100

    ...

--- swiftShift/src/pages/Home.jsx
+++ swiftShift/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Nav from "../components/Nav";
 import "./Home.css";
 import Header from "../components/Header";
+import Trusted from "../components/Trusted";
+import Card from "../components/Card";
 
 function Home() {
   return (
@@ -12,6 +14,12 @@ function Home() {
           <Header
             title="SwiftShift"
             description="Streamline Your Workflow with Our Secure, User-Friendly Product/Service - Seamlessly Integrated with Other Tools."
+          />
+          <Trusted title="Trusted by" />
+          <Card
+            title="User-friendly interface"
+            description="Our product/service has an intuitive design, making it easy for users
+          to navigate and access the features they need."
           />
         </div>
       </div>
