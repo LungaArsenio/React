2435cab807804e59e6f7f3ad5605db7fbd81988f
lunga <lungaarsenio0@gmail.com>
2023-05-03 03:49:50 +0100

    making the image in this component dynamic receiving props for the src of the image

--- swiftShift/src/components/Header.jsx
+++ swiftShift/src/components/Header.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./Header.css";
 
-function Header({ title, description }) {
+function Header({ title, description, image, hideButtons }) {
   return (
     <div className="header">
       <h1 className="header__title">{title}</h1>
       <p className="header__description"> {description}</p>
-      <div className="header__btn">
-        <button className="header__btn--left">Join us now</button>
+      <div className={!hideButtons ? `header__btn` : `header__btn hide`}>
+        <button className="header__btn--left ">Join us now</button>
         <button className="header__btn--right">Request demo</button>
       </div>
 
       <figure className="header__img-container">
-        <img className="header__img" src="./src/assets/teampicture.jpg" />
+        <img className="header__img" src={image} />
       </figure>
     </div>
   );
