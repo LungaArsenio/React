9bcbf57cf9d5bb667287e2d2997940d871cb9571
lunga <lungaarsenio0@gmail.com>
2023-05-06 09:04:23 +0100

    ...

--- swiftShift/src/App.jsx
+++ swiftShift/src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Features from "./pages/Features";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -9,10 +13,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/Features" element={<Features />} />
+          <Route path="/Features" element={<Features />} />
           <Route path="/About" element={<About />} />
           <Route path="/Blog" element={<Blog />} />
-          <Route path="/Contact" element={<Contact />} /> */}
+          <Route path="/Contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
