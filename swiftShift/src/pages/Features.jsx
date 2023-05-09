ff99fbb03176f8c80ecbadef3a790ca84318723a
lunga <lungaarsenio0@gmail.com>
2023-05-09 02:54:09 +0100

    adding header

--- swiftShift/src/pages/Features.jsx
+++ swiftShift/src/pages/Features.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import FeatureCard from "../components/FeatureCard";
 
 function Features() {
-  return <h1>Features</h1>;
+  const hideButtons = true;
+  return (
+    <>
+      <Nav />
+      <div className="container">
+        <div className="row">
+          <Header
+            title="Real-time data analysis"
+            description="Our product/service uses advanced
+          algorithms to analyze data in real-time, providing users
+          with up-to-date insights and recommendations."
+            image="./src/assets/blog3.jpg"
+            hideButtons
+          />
+
+          <FeatureCard
+            title="Automated processes "
+            description="Our product/service automates repetitive tasks,
+            freeing up users' time and reducing the risk of human error."
+            image="./src/assets/twogirlsworking.jpg"
+          />
+
+          <FeatureCard
+            reverse
+            title="Integration with other tools "
+            description="Our product/service seamlessly integrates
+             with other popular tools and platforms,
+             allowing users to streamline their workflows."
+            image="./src/assets/menandwomenworking.jpg"
+          />
+
+          <FeatureCard
+            title="24/7 customer support "
+            description=" Our knowledgeable and responsive customer support
+            team is available around the clock to assist users
+             with any questions or issues they may have."
+            image="./src/assets/workinginboard.jpeg"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Features;
