c1ef79ac367adb0535ea21efb3b423e89e8c1ef1
lunga <lungaarsenio0@gmail.com>
2023-05-14 20:38:29 +0100

    updating pages

--- swiftShift/src/pages/Contact.jsx
+++ swiftShift/src/pages/Contact.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Nav from "../components/Nav";
 
 function Contact() {
-  return <h1>Contact</h1>;
+  return <Nav />;
 }
 
 export default Contact;
--- swiftShift/src/pages/Home.jsx
+++ swiftShift/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Nav from "../components/Nav";
-import "./Home.css";
+import "../App.css";
 import Header from "../components/Header";
 import Trusted from "../components/Trusted";
 import FeatureCard from "../components/FeatureCard";
-// import FeatureCardReversed from "../components/FeatureCardReversed";
+import Pricing from "../components/Pricing";
 
 function Home() {
   const reverse = true;
@@ -16,19 +16,23 @@ function Home() {
           <Header
             title="SwiftShift"
             description="Streamline Your Workflow with Our Secure, User-Friendly Product/Service - Seamlessly Integrated with Other Tools."
+            image="./src/assets/teampicture.jpg"
           />
           <Trusted title="Trusted by" />
           <FeatureCard
             title="User-friendly interface"
             description="Our product/service has an intuitive design, making it easy for users
           to navigate and access the features they need."
+            image="./src/assets/twogirlsworking.jpg"
           />
           <FeatureCard
             reverse={reverse}
             title="Customizable settings"
             description="Users can tailor the product/service to meet
             their specific needs and preferences."
+            image="./src/assets/menandwomenworking.jpg"
           />
+          <Pricing />
         </div>
       </div>
     </>
