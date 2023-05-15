7180f02e6b3689909924087a6d73ab9556ce22ed
lunga <lungaarsenio0@gmail.com>
2023-05-16 00:21:54 +0100

    fontawesome setup

--- swiftShift/src/components/PricingCard.jsx
+++ swiftShift/src/components/PricingCard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "../components/PricingCard.css";
-
-function PricingCard({ title, description, price, time, main }) {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+function PricingCard({ title, description, price, time }) {
   return (
     <div className="pricingCard">
       <div className="pricingCard__header">
@@ -12,7 +13,9 @@ function PricingCard({ title, description, price, time, main }) {
           <span className="pricingCard__header__pricing--time">{time}</span>
         </div>
       </div>
-      <div className="pricingCard__features"></div>
+      <div className="pricingCard__features">
+        <FontAwesomeIcon icon={faEnvelope} />
+      </div>
     </div>
   );
 }
