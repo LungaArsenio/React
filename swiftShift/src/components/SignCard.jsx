935df6104fdaa85204f45362e7ac0da2fed000a9
lunga <lungaarsenio0@gmail.com>
2023-05-22 17:52:54 +0100

    spacing inside of card description

--- swiftShift/src/components/SignCard.jsx
+++ swiftShift/src/components/SignCard.jsx
@@ -7,10 +7,10 @@ function SignCard({ image }) {
       <div className="signCard__description">
         <p className="signCard__description--title">Get started</p>
         <p className="signCard__description--sub-title">
-          Do consecteur proident proident id eiusod deserunt consequat pariatur
-          ad ex velit do Lore reprehendit
+          Do consecteur proident proident id eiusmod deserunt consequat pariatur
+          ad ex velit do Lorem reprehenderit.
         </p>
-        <button className="signCard__btn">Sign in</button>
+        <button className="signCard__btn">Sign up</button>
       </div>
       <figure className="signCard__container">
         <img className="signCard__image" src={image} alt="" />
