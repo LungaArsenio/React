6d9c09c0454fa70721c3c3d5780d3b203e325d26
lunga <lungaarsenio0@gmail.com>
2023-06-16 19:28:30 +0100

    Trusted section

--- swiftShift/src/components/Trusted.jsx
+++ swiftShift/src/components/Trusted.jsx
@@ -6,31 +6,21 @@ function Trusted({ title }) {
     <div className="trusted">
       <h3 className="trusted__title">{title}</h3>
       <div className="trusted__row">
-        <img
-          className="trusted__row__img"
-          src="./src/assets/molestie.svg"
-          alt="Company logo"
-        />
-        <img
-          className="trusted__row__img"
-          src="./src/assets/egestas.svg"
-          alt="Company logo"
-        />
-        <img
-          className="trusted__row__img"
-          src="./src/assets/torquent.svg"
-          alt="Company logo"
-        />
-        <img
-          className="trusted__row__img"
-          src="./src/assets/dolor.svg"
-          alt="Company logo"
-        />
-        <img
-          className="trusted__row__img"
-          src="./src/assets/sociousqu.svg"
-          alt="Company logo"
-        />
+        <figure className="trusted__row__img">
+          <img src="./src/assets/molestie.svg" alt="Company logo" />
+        </figure>
+        <figure className="trusted__row__img">
+          <img src="./src/assets/egestas.svg" alt="Company logo" />
+        </figure>
+        <figure className="trusted__row__img">
+          <img src="./src/assets/torquent.svg" alt="Company logo" />
+        </figure>
+        <figure className="trusted__row__img">
+          <img src="./src/assets/dolor.svg" alt="Company logo" />
+        </figure>
+        <figure className="trusted__row__img">
+          <img src="./src/assets/sociousqu.svg" alt="Company logo" />
+        </figure>
       </div>
     </div>
   );
