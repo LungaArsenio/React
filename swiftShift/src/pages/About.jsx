ea76521d77bef0dddbb23a2775a52844ffdf0138
lunga <lungaarsenio0@gmail.com>
2023-06-20 20:47:41 +0100

    About us header

--- swiftShift/src/pages/About.jsx
+++ swiftShift/src/pages/About.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import Nav from "../components/Nav";
+import Header from "../components/Header";
 
 function About() {
-  return <Nav />;
+  return (
+    <>
+      <Nav />
+      <div className="container">
+        <div className="row">
+          <Header
+            hideButtons={true}
+            title={"About us"}
+            description={
+              "With advanced security measures and seamless integration with other tools, our product/service is designed to streamline your workflow and help you work more efficiently."
+            }
+            image={"./src/assets/teampicture.jpg"}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
