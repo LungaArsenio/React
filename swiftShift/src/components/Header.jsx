ee64d84387add5099ce6a6a5134625bceb8e5d50
lunga <lungaarsenio0@gmail.com>
2023-06-20 20:40:13 +0100

    Conditional styling, image shadow

--- swiftShift/src/components/Header.jsx
+++ swiftShift/src/components/Header.jsx
@@ -4,15 +4,26 @@ import "./Header.css";
 function Header({ title, description, image, hideButtons }) {
   return (
     <div className="header">
-      <h1 className="header__title">{title}</h1>
-      <p className="header__description"> {description}</p>
+      <h1 className={!title ? "header__title hide" : "header__title"}>
+        {title}
+      </h1>
+      <p
+        className={
+          !description ? "header__description hide" : "header__description"
+        }
+      >
+        {description}
+      </p>
       <div className={!hideButtons ? `header__btn` : `header__btn hide`}>
         <button className="header__btn--left ">Join us now</button>
         <button className="header__btn--right">Request demo</button>
       </div>
 
       <figure className="header__img-container">
-        <img className="header__img" src={image} />
+        <img
+          className={hideButtons ? "header__img" : "header__img contrast"}
+          src={image}
+        />
       </figure>
     </div>
   );
