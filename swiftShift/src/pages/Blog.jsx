88fd3481f2d35b8a977ac899c5df6f2a4dad01e1
lunga <lungaarsenio0@gmail.com>
2023-06-20 20:49:33 +0100

    Blog header, blogs, sign card

--- swiftShift/src/pages/Blog.jsx
+++ swiftShift/src/pages/Blog.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Nav from "../components/Nav";
+import Header from "../components/Header";
+import Blogs from "../components/Blogs";
+import SignCard from "../components/SignCard";
 
 function Blog() {
   return (
     <>
       <Nav />
+      <div className="container">
+        <div className="row">
+          <Header hideButtons={true} image={"./src/assets/bigblogpic.jpg"} />
+          <Blogs numCards={6} />
+          <SignCard image={"./src/assets/girlsmilling.jpg"} />
+        </div>
+      </div>
     </>
   );
 }
